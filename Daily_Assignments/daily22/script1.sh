ecaa1ba94753138b1bac42403f22cbfb77843c25
Amir Katorza <amir.katorza@gmail.com>
2023-03-20 23:01:00 +0200

    Fixed the script in daily22

--- Daily_Assignments/daily22/script1.sh
+++ Daily_Assignments/daily22/script1.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 
-i=1
+# Create a new dir named “dir”. 
+# Create new files named “file1.txt” to “file9.txt” in “dir”
+# Write a script that renames file1.txt file2.txt etc 
+# to “document1” to “document9” using loops.
+
+# Create Directory and files
+$(mkdir dir)
+$(touch dir/file{1..9}.txt)
+
+# Change the names
 for f in dir/* 
 do
-    $(mv $f dir/document$i.txt)
-    (( i++ ))
+    file_num=$(echo $f | sed 's/[^0-9]*//g')
+    $(mv $f dir/document$file_num.txt)    
 done
